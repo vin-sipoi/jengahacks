5d4a9d94944f4609e422081daccbdca60efe7a74
katekuehl <katekuehl@gmail.com>
2025-12-26 13:55:09 -0600

    feat: Add React, CVA, and utility imports to badge and toggle components.

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,3 +1,6 @@
+import * as React from "react";
+import { type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 import { badgeVariants } from "./badge-variants";
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
--- src/components/ui/toggle.tsx
+++ src/components/ui/toggle.tsx
@@ -1,3 +1,7 @@
+import * as React from "react";
+import * as TogglePrimitive from "@radix-ui/react-toggle";
+import { type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
 import { toggleVariants } from "./toggle-variants";
 
 const Toggle = React.forwardRef<
